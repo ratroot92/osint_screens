e52960552f592483d659de0ec2bfe956b3ef8278
ratroot92 <zcomisb.007@gmail.com>
2020-03-11 17:39:47 +0500

    asd

---
+++ case_overview_case_screen_final/js/additional_chart.js
@@ -0,0 +1,35 @@
+// $('#caseOverview_div').hide();
+
+
+// $('#caseOverview_btn').on('click',function(){
+//   $('#caseCreate_div').hide();
+//   $('#caseOverview_div').show();
+// });
+
+
+
+// $('#caseCreate_btn').on('click',function(){
+//   $('#caseOverview_div').hide();
+//   $('#caseCreate_div').show();
+  
+// });
+
+
+
+
+
+$('#caseCreate_div').hide();
+
+
+$('#caseOverview_btn').on('click',function(){
+  $('#caseCreate_div').hide();
+  $('#caseOverview_div').show();
+});
+
+
+
+$('#caseCreate_btn').on('click',function(){
+  $('#caseOverview_div').hide();
+  $('#caseCreate_div').show();
+  
+});
